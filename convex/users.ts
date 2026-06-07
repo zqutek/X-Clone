f560d36fa58230afcfc0b04a265ff20428a13d72
zqutek <nbi41204@gmail.com>
2026-06-07 20:43:48 +0200

    feat: add Clerk webhook user creation

---
+++ convex/users.ts
@@ -0,0 +1,33 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+export const createUser = mutation({
+  args: {
+    username: v.string(),
+    fullname: v.string(),
+    email: v.string(),
+    bio: v.optional(v.string()),
+    image: v.string(),
+    clerkId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const existingUser = await ctx.db
+      .query("users")
+      .withIndex("by_clerk_id", (q) => q.eq("clerkId", args.clerkId))
+      .first();
+
+    if (existingUser) return;
+
+    await ctx.db.insert("users", {
+      username: args.username,
+      fullname: args.fullname,
+      email: args.email,
+      bio: args.bio,
+      image: args.image,
+      clerkId: args.clerkId,
+      followers: 0,
+      following: 0,
+      posts: 0,
+    });
+  },
+});
